abd537eb1c003d858cc61547b4aea6e9bdc25ffd
SamuelBalani <samuelbalanibergara@yahoo.com.br>
2023-09-01 21:04:01 -0300

    Corrigido codigo "ifElseIf.js"

--- exercicios-web/controle/ifElseIf.js
+++ exercicios-web/controle/ifElseIf.js
@@ -2,8 +2,8 @@ Number.prototype.entre = function (inicio, fim) {
     return this >= inicio && this <= fim
 }
 
-const imprimiResultado = function (nota) {
-    if(NodeIterator.entr(9, 10)){
+const imprimirResultado = function (nota) {
+    if(nota.entre(9, 10)){
         console.log('Quadro de Honra')
     }else if(nota.entre(7, 8.99)) {
         console.log('Aprovado')
